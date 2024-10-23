f4eba6370d2929ec6a331876d308a0fbfeafcc10
kbushi <kbushi@KBUSHI>
2024-10-23 20:34:48 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication2/Controllers/JsonViewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication2.Controllers
+{
+    public class JsonViewController : Controller
+    {
+        // JSONをパースするためのサンプルクラス
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Email { get; set; }
+        }
+
+        public IActionResult Index()
+        {
+            // サンプルJSON文字列
+            string jsonString = "{\"Name\":\"山田太郎\",\"Age\":25,\"Email\":\"taro@example.com\"}";
+
+            // JSONをC#オブジェクトにパース（デシリアライズ）
+            Person person = JsonConvert.DeserializeObject<Person>(jsonString);
+            ViewData["ParsedJson"] = $"名前: {person.Name}, 年齢: {person.Age}, メール: {person.Email}";
+
+            // C#オブジェクトをJSON文字列に変換（シリアライズ）
+            var newPerson = new Person { Name = "鈴木花子", Age = 28, Email = "hanako@example.com" };
+            string serializedObject = JsonConvert.SerializeObject(newPerson, Formatting.Indented);
+            ViewData["SerializedObject"] = serializedObject;
+
+            return View();
+        }
+    }
+}
